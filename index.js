d1923eaf9a7ecd4db4b270fff7c91935feb363bd
isaias <isaias.villa@student.uncommonhs.org>
2017-02-08 22:26:06 +0000

    i changed the input

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ $(document).ready( function(){
     $('#change-color').click(function(){
         $('body').css("background-color","red");
     })
+    $("#submit-name").click(function() {
+        var name=$("#name").val();
+        
+    });
+        
+    $("#submit-name").click(function(){
+       
+        $("#greeting").append("#name")
+    });
 })
 
